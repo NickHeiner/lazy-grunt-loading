6ccd6ab478ec5ac30aa2f40654fc75270b71cec0
Nick Heiner <nick.heiner@opower.com>
2013-12-08 15:40:41 -0500

    adding another test

--- test/unit/core/get-task-names.test.js
+++ test/unit/core/get-task-names.test.js
@@ -5,9 +5,14 @@ var getTaskNames = require('../../../lib/core/get-task-names'),
 
 describe('get-task-names', function() {
 
-    it('should return the argument unchanged when it is a single string without a colon', function() {
+    it('should return the argument as a singleton list when it is a single string without a colon', function() {
         var toRun = 'jshint';
         expect(getTaskNames(toRun)).to.deep.equal([toRun]);
+    });
+
+    it('should return the argument unchanged when is a list of tasks with no colons', function() {
+        var toRun = ['jshint', 'foo-task', 'bar-task'];
+        expect(getTaskNames(toRun)).to.deep.equal(toRun);
     });
 
 });
