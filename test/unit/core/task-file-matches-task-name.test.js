b0343954239f80cb469e7baa55a20fd1a2fc0fc5
Nick Heiner <nick.heiner@opower.com>
2013-12-08 15:50:47 -0500

    adding more tests

--- test/unit/core/task-file-matches-task-name.test.js
+++ test/unit/core/task-file-matches-task-name.test.js
@@ -13,4 +13,16 @@ describe('task-file-matches-task-name', function() {
         expect(taskFileMatchesTaskName('sass', null, 'node_modules/grunt-sass/tasks/sass.js')).to.equal(true);
     });
 
+    it('handles a "grunt-" prefix', function() {
+        expect(taskFileMatchesTaskName('sass', {}, 'node_modules/grunt-sass/tasks/grunt-sass.js')).to.equal(true);
+    });
+
+    it('checks the overrides', function() {
+        expect(taskFileMatchesTaskName(
+            'sass',
+            {'my-fun-with-sass': 'sass'},
+            'node_modules/grunt-sass/tasks/my-fun-with-sass.js')
+        ).to.equal(true);
+    });
+
 });
