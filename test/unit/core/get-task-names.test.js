26e48aa04ec3fdef4e796e6039673aa1b8722de9
Nick Heiner <nick.heiner@opower.com>
2013-12-08 15:30:49 -0500

    first passing test

--- test/unit/core/get-task-names.test.js
+++ test/unit/core/get-task-names.test.js
@@ -1,5 +1,13 @@
 'use strict';
 
+var getTaskNames = require('../../../lib/core/get-task-names'),
+    expect = require('chai').expect;
+
 describe('get-task-names', function() {
+
+    it('should return the argument unchanged when it is a single string without a colon', function() {
+        var toRun = 'jshint';
+        expect(getTaskNames(toRun)).to.deep.equal([toRun]);
+    });
 
 });
