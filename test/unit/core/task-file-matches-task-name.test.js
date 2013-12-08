25f535cd9082343763be8860d06c1213e9389056
Nick Heiner <nick.heiner@opower.com>
2013-12-08 15:47:38 -0500

    making t-f-w-t-n more robust

--- test/unit/core/task-file-matches-task-name.test.js
+++ test/unit/core/task-file-matches-task-name.test.js
@@ -9,6 +9,8 @@ describe('task-file-matches-task-name', function() {
         expect(taskFileMatchesTaskName('sass', {}, 'node_modules/grunt-sass/tasks/sass.js')).to.equal(true);
     });
 
-    it('handles no overrides being passed');
+    it('handles no overrides being passed', function() {
+        expect(taskFileMatchesTaskName('sass', null, 'node_modules/grunt-sass/tasks/sass.js')).to.equal(true);
+    });
 
 });
